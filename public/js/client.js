0fe62b0d54a6706bb220369167e07ef2dc3c5124
DarkSurfer235 <Darksurfer235@gmail.com>
2022-08-18 20:09:38 +0530

    Initial commit

---
+++ public/js/client.js
@@ -0,0 +1,80 @@
+const socket = io();
+
+let username;
+let chats = document.querySelector('.chats');
+let users_list = document.querySelector('.users-list');
+let users_count = document.querySelector('.users-count');
+let user_msg = document.querySelector('#user-msg');
+let msg_send = document.querySelector('#msg-send');
+
+do{
+    username = prompt ('Enter your name: ');   
+}while(!username);
+
+// called when user will joined
+socket.emit('new-user-joined', username);
+
+// notify others that user has joined
+socket.on('user-connected',(socket_name) => {
+    userJoinLeft(socket_name, 'joined');   
+})
+
+// function to create join/left status
+function userJoinLeft(name,status) {
+    let div = document.createElement('div');
+    div.classList.add('user-join');
+    let content = `<p><b>${name}</b> ${status} the chat</p>`;
+    div.innerHTML = content;
+    chats.appendChild(div);
+    chats.scrollTop = chats.scrollHeight;
+}
+
+// notify others that user has left
+socket.on('user-disconnected',(user) => {
+    userJoinLeft(user, 'left');
+})
+
+
+// for updating user list and their count
+socket.on('user-list', (users)=>{
+    users_list.innerHTML='';
+    users_arr=Object.values(users);
+    for(i=0; i<users_arr.length; i++){
+        let p=document.createElement('p');
+        p.innerText=users_arr[i];
+        users_list.appendChild(p);
+    }
+
+    users_count.innerHTML=users_arr.length;
+})
+
+
+// for sending message
+
+msg_send.addEventListener('click',()=>{
+    let data = {
+        user: username,
+        msg: user_msg.value
+    };
+    if(user_msg.value!=''){
+        appendMessage(data, 'outgoing');
+        socket.emit('message', data);
+        user_msg.value = '';
+    };
+});
+
+function appendMessage(data, status) {
+    let div = document.createElement('div');
+    div.classList.add('message', status);
+    let content = `
+    <h5>${data.user}</h5>
+    <p>${data.msg}</p>
+    `;
+    div.innerHTML = content;
+    chats.appendChild(div);
+    chats.scrollTop = chats.scrollHeight;
+}
+
+socket.on('message',(data)=>{
+    appendMessage(data, 'incoming');
+});
